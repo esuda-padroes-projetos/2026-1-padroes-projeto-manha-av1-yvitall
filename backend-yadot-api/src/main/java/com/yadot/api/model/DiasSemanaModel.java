ee9972579fe4b8f2171aa54a42a4ab35b57e2032
yvitall <yuritsvital05@gmail.com>
2026-03-13 00:39:17 -0300

    create: categoriaModel, EnumDiasSemana // modelagem da tabela com @manytoone e mais uma caralhada de coisas, entendendo o que faz as annotations do SPRING

---
+++ backend-yadot-api/src/main/java/com/yadot/api/model/DiasSemanaModel.java
@@ -0,0 +1,5 @@
+package com.yadot.api.model;
+
+public enum DiasSemanaModel {
+    SEGUNDA, TERCA, QUARTA, QUINTA, SEXTA, SABADO, DOMINGO
+}
--- backend-yadot-api/src/main/java/com/yadot/api/model/HabitModel.java
+++ backend-yadot-api/src/main/java/com/yadot/api/model/HabitModel.java
@@ -4,6 +4,8 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.List;
+
 @Getter @Setter // Lombok
 @Entity // Transforma a classe em uma entidade no banco de dados (necessario em classesModel/Entities)
 @Table(name = "tb_habitos") //criacao de uma tabela com nome da tabela
@@ -11,17 +13,33 @@ import lombok.Setter;
 public class HabitModel {
     @Id //add auto na tabela o id (dependency JavaPersistenceAPI/BD)
     @GeneratedValue(strategy = GenerationType.IDENTITY) //forma de gerar o id (1, 2...)
+
     private Long habitId;
+
     private String habitName;
-    private String habitCategory;
+
+    // muitos hábitos podem ter a mesma categoria
+    @ManyToOne
+    @JoinColumn(name = "categoria_id") // precisamos juntar as duas tabelas (FK)
+    private CategoriaModel categoria;
+
     private String habitIcon;
+
+    private boolean saveGoogleCalendar;
+
+    @ElementCollection(targetClass = DiasSemanaModel.class)
+    @Enumerated(EnumType.STRING) // ao inves de armazenar os numeros(indices do enum) salvaremos a string contendo dia da semana
+    @CollectionTable(name = "habitos_dias", joinColumns = @JoinColumn(name = "habit_id"))
+    private List<DiasSemanaModel> diasDaSemana;
 
     public HabitModel() {
     }
 
-    public HabitModel(String habitName, String habitCategory, String habitIcon) {
+    public HabitModel(String habitName, CategoriaModel categoria, String habitIcon,  boolean saveGoogleCalendar,  List<DiasSemanaModel> diasDaSemana) {
         this.habitName = habitName;
-        this.habitCategory = habitCategory;
+        this.categoria = categoria;
         this.habitIcon = habitIcon;
+        this.saveGoogleCalendar = saveGoogleCalendar;
+        this.diasDaSemana = diasDaSemana;
     }
 }
